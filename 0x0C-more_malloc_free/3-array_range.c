0527f851b627b9bafdaa1df31ef80168ccea3f82
kevinmziyako <kevinmziyako@gmail.com>
2023-04-13 14:42:51 -0700

    more_malloc_free2

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,10 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 
-
-
 /**
-
  * *array_range - creates an array of integers
  * @min: minimum range of values stored
  * @max: maximum range of values stored and number of elements
